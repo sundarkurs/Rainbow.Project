d38b5f4534561312240dc3dbd1d2c16b1189cd63
Sundar Urs <sundarkurs@gmail.com>
2019-09-23 10:50:20 +0530

    Added projects for Header and Footer components under Feature layer

---
+++ src/Feature/Header/code/App_Start/RazorGeneratorMvcStart.cs
@@ -0,0 +1,21 @@
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using RazorGenerator.Mvc;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Rain.Feature.Header.RazorGeneratorMvcStart), "Start")]
+
+namespace Rain.Feature.Header {
+    public static class RazorGeneratorMvcStart {
+        public static void Start() {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
+                UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
+            };
+
+            ViewEngines.Engines.Insert(0, engine);
+
+            // StartPage lookups are done by WebPages. 
+            VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
+        }
+    }
+}
