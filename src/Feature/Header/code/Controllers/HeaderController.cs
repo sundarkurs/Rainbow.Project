68ebaa0ab09652d938a2df8ca32c271970e50977
Sundar Urs <sundarkurs@gmail.com>
2019-12-23 19:21:34 +0530

    Added top header component

--- src/Feature/Header/code/Controllers/HeaderController.cs
+++ src/Feature/Header/code/Controllers/HeaderController.cs
@@ -1,22 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Rain.Feature.Header.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
 
 namespace Rain.Feature.Header.Controllers
 {
     public class HeaderController : Controller
     {
-        // GET: Header
-        public ActionResult Header()
+
+        public ActionResult MainHeader()
         {
             return View();
         }
 
-        public ActionResult HeaderWithLogo()
+        public ActionResult MainHeaderWithLinks()
         {
-            return View("~/Views/Header/HeaderWithLogo.cshtml");
+            return View();
+        }
+
+        public ActionResult TopHeader()
+        {
+            var viewModel = new List<_GenericLink>();
+
+            var dataSourceItem = DataSourceItem;
+            if (dataSourceItem != null)
+            {
+                var navigationLinks = dataSourceItem.Children;
+
+                if (navigationLinks != null || navigationLinks.Count > 0)
+                {
+                    navigationLinks.ToList().ForEach(x =>
+                    {
+                        viewModel.Add(new _GenericLink()
+                        {
+                            Title = x.Fields["Title"].ToString(),
+                            Link = (LinkField)x.Fields["Link"]
+                        });
+                    });
+                }
+            }
+
+            return View(viewModel);
+        }
+
+        public Item DataSourceItem
+        {
+            get
+            {
+                try
+                {
+                    var str = RenderingContext.Current.Rendering.DataSource;
+                    return string.IsNullOrEmpty(str) ? null : RenderingContext.Current.Rendering.Item;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
         }
     }
 }
---
+++ src/Project/Website/code/Personalization/WebsiteDomains.cs
@@ -0,0 +1,37 @@
+﻿using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Sites;
+
+namespace Rain.Project.Website.Personalization
+{
+    public class WebsiteDomains<T> : Sitecore.Rules.Conditions.SiteConditions.WebSiteNameCondition<T>
+        where T : RuleContext
+    {
+
+        protected override bool Execute(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+
+            if (string.IsNullOrEmpty(base.Value))
+            {
+                return false;
+            }
+
+            var siteItem = Context.Database.GetItem(base.Value);
+            if (siteItem == null)
+            {
+                return false;
+            }
+
+            var contextSite = Context.Site;
+            if (contextSite == null)
+            {
+                return false;
+            }
+
+            return Compare(contextSite.Name, siteItem.Name);
+        }
+
+    }
+}
