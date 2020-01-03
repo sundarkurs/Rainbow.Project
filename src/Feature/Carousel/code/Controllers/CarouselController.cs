1629349dcc7d7272d7748c16fe69cd58f7862cbf
Sundar Urs <sundarkurs@gmail.com>
2020-01-03 18:44:36 +0530

    Feature Carousel implementation

---
+++ src/Feature/Carousel/code/Controllers/CarouselController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Rain.Feature.Carousel.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace Rain.Feature.Carousel.Controllers
+{
+    public class CarouselController : Controller
+    {
+        public ActionResult Slides()
+        {
+            var dataSourceItem = DataSourceItem;
+            if (dataSourceItem != null)
+            {
+
+            }
+            
+            return View(dataSourceItem);
+        }
+
+        public Item DataSourceItem
+        {
+            get
+            {
+                try
+                {
+                    var str = RenderingContext.Current.Rendering.DataSource;
+                    return string.IsNullOrEmpty(str) ? null : RenderingContext.Current.Rendering.Item;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ src/Feature/Carousel/code/Models/_Carousel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rain.Feature.Carousel.Models
+{
+    public class _Carousel
+    {
+    }
+}
